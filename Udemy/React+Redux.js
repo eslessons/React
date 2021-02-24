ffad754fc5aeb757ca1559f173c24b9764c38aed
ES Lessons <eslessons@tutanota.com>
2021-02-24 18:15:41 +0300

    Start Udemy - React + Redux (2019) -> 1. Introduction

---
+++ Udemy/React+Redux.js
@@ -0,0 +1,22 @@
+/* Udemy - 
+React + Redux (2019)
+
+1. Introduction
+Start
+
+
+2. ECMAScript 2019
+3. React Basics
+4. Components State and Events Handling
+5. Finishing Todo App
+6. Working with Server
+7. Components lifecycle
+8. React Patterns
+9. React Hooks
+10. React Router
+11. Redux
+12. Creating Redux Application
+13. Building React Application - Babel
+14. Building React Application - Webpack
+
+*/
