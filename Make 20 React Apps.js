c2964a8cd1217f2fdd6efa296516d7804ce5c46b
ES Lessons <eslessons@tutanota.com>
2021-03-09 13:13:53 +0300

    Finish 00. Getting Started

---
+++ Make 20 React Apps.js
@@ -0,0 +1,27 @@
+/* Make 20 React Apps 
+
+00. Getting Started
+Start
+Finish
+
+01. Pomodoro Timer
+02. Markdown Editor
+03. Browser Tabs and Routing
+04. Paper Rock Scissors
+05. Moving Link Across a _canvas_
+06. Infinite Image Gallery
+07. Trivia Game
+08. Authentication
+09. Web Speech and Timers
+10. Calendar Picker
+11. Chat Box
+12. Multi-Step Form
+13. Memory Matching Game
+14. Drag and Drop Math Card
+15. Browser
+16. Real-Time Search
+17. Charting the Weather
+18. Shopping Cart w_ Context + Reducer
+19. Typing Tester
+
+*/
