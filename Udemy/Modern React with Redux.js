8e3cbbc4295b06f302e3b6104cded206c2bd210c
ES Lessons <eslessons@tutanota.com>
2021-03-15 08:29:33 +0300

    Start Udemy - Modern React with Redux -> 01. Let's Dive In!

---
+++ Udemy/Modern React with Redux.js
@@ -0,0 +1,45 @@
+/* Udemy - 
+Stephen Grider - 
+Modern React with Redux
+
+01. Let's Dive In!
+Start
+
+02. Building Content with JSX
+03. Communicating with Props
+04. Structuring Apps with Class-Based Components
+05. State in React Components
+06. Understanding Lifecycle Methods
+07. Handling User Input with Forms and Events
+08. Making API Requests with React
+09. Building Lists of Records
+10. Using Ref's for DOM Access
+11. Let's Test Your React Mastery!
+12. Understanding Hooks in React
+13. Navigation From Scratch
+14. Hooks in Practice
+15. Deploying a React App
+16. On We Go...To Redux!
+17. Integrating React with Redux
+18. Async Actions with Redux Thunk
+19. Redux Store Design
+20. Navigation with React Router
+21. Handling Authentication with React
+22. Redux Dev Tools
+23. Handling Forms with Redux Form
+24. REST-Based React Apps
+25. Using React Portals
+26. Implementing Streaming Video
+27. The Context System with React
+28. Replacing Redux with Context
+29. Working with Older Versions of React
+30. Ajax Requests with React
+31. Modeling Application State
+32. Managing App State with Redux
+33. Intermediate Redux_ Middleware
+34. React Router + Redux Form v6
+35. Bonus Topics
+36. React Router + Redux Form v4
+37. Extras
+
+*/
