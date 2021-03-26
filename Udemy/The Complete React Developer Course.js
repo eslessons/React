130cf212f6e880748e3f92957307ed23f986e823
ES Lessons <eslessons@tutanota.com>
2021-03-26 08:35:55 +0300

    Finish 9. React-Router

--- Udemy/The Complete React Developer Course.js
+++ Udemy/The Complete React Developer Course.js
@@ -29,22 +29,22 @@ Finish
 Start
 Finish
 
-8. Styling React 
+8. Styling React
 Start
 Finish
 
-9. React-Router 
+9. React-Router
 Start
+Finish
 
-
-10. Redux 
-11. React with Redux 
-12. Testing Your Application 
-13. Deploying Your Apps 
-14. Firebase 101 
-15. Firebase with Redux 
-16. Firebase Authentication 
-17. Styling Budget App 
-18. What Now 
-19. Hooks, Context, Fragments, and More 
+10. Redux
+11. React with Redux
+12. Testing Your Application
+13. Deploying Your Apps
+14. Firebase 101
+15. Firebase with Redux
+16. Firebase Authentication
+17. Styling Budget App
+18. What Now
+19. Hooks, Context, Fragments, and More
 
