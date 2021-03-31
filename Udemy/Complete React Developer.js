86d29547b0c9422db7b6e8b3d70c7cd6158e1563
ES Lessons <eslessons@tutanota.com>
2021-03-31 18:45:53 +0300

    Start Udemy - Complete React Developer Zero-to-Mastery -> 01. Introduction

---
+++ Udemy/Complete React Developer.js
@@ -0,0 +1,42 @@
+/* Udemy - 
+Complete React Developer Zero-to-Mastery
+
+01. Introduction 
+Start
+
+
+02. React Key Concepts 
+03. React Basics 
+04. Master Project Setting Up E-commerce Project 
+05. Master Project React Router and Routing 
+06. Master Project Forms + Components 
+07. Master Project Firebase + User Authentication 
+08. Master Project Redux 1 
+09. Master Project Session Storage + Persistence 
+10. Master Project Redux 2 
+11. Master Project Advanced Routing 
+12. Master Project State Normalization 
+13. Master Project Stripe Payments Part 1 
+14. Master Project Deploying To Production 
+15. Master Project CSS in JS - styled-components 
+16. Master Project Advanced Redux + Firebase 
+17. Master Project HOC Patterns 
+18. Master Project  Asynchronous Redux 
+19. Master Project Container Pattern 
+20. Master Project Redux-Saga 
+21. Master Project React Hooks 
+22. Master Project Stripe Payments Part 2 - Backend 
+23. Master Project Context API 
+24. Master Project GraphQL + Apollo 
+25. Master Project Mobile Support 
+26. Master Project React Performance 
+27. React Interview Questions + Advice 
+28. Bonus Progressive Web App 
+29. Bonus Firebase Security 
+30. Bonus Testing 
+31. Bonus Webpack + Babel 
+32. Bonus Build a GatsbyJS Blog 
+33. Appendix 1 Key Developer Concepts 
+34. Open Source Projects 
+35. AMA + Bonus 
+36. Extras
